d4725401d98a583268423b767f33e9d9db1bb849
calvinchiff <calel44jke@gmail.com>
2025-03-31 15:14:05 +0200

    feat: careerSection Timeline element clickable to show details in details tile

--- app/components/sections/CareerSection.tsx
+++ app/components/sections/CareerSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Tile from "@/app/components/ui/Tile";
 import Section from "@/app/components/ui/Section";
@@ -6,14 +6,69 @@ import { careerData } from "@/public/data/careerData";
 import { useLanguage } from "@/app/components/lib/LanguageContext";
 
 export default function CareerSection() {
+	const { language } = useLanguage();
+	const [active, setActive] = useState(careerData.entries[0]);
 	return (
-		<Section id="career" nbLeftGridRows={2} nbRightGridRows={2}>
-			<Tile title="Timeline" customClassName="row-span-2">
-				LTTD Consulting
-			</Tile>
-			<Tile title="Details" customClassName="col-span-2 row-span-2">
-				LTTD Consulting 2023-24
-			</Tile>
+		<Section
+			id="career"
+			customGridClassName="flex flex-col md:flex-row"
+			nbLeftGridRows={2}
+			nbRightGridRows={2}
+		>
+			<div className="h-full basis-1/3">
+				<Tile
+					title={careerData.tiles.timeline.name[language]}
+					customClassName=""
+				>
+					<ul>
+						{careerData.entries.map((entry, index) => (
+							<li
+								key={entry.id}
+								className={`cursor-pointer transition-all duration-150 ${
+									active === entry
+										? "opacity-90 text-glow"
+										: "opacity-40 hover:opacity-60"
+								}`}
+								onClick={() => setActive(entry)}
+							>
+								<h3>
+									{entry.from.split("-")[2]} - {entry.to.split("-")[2]}
+								</h3>
+								{entry.title[language]}
+							</li>
+						))}
+					</ul>
+				</Tile>
+			</div>
+			<div className="h-full basis-2/3">
+				<Tile
+					title={careerData.tiles.details.name[language]}
+					customClassName=""
+				>
+					<div className="flex flex-col md:gap-2 md:mt-2">
+						{Object.entries(careerData.tiles.details.contentTitles).map(
+							([key, label]) => {
+								const value = active[key];
+
+								if (!value || (typeof value === "object" && !value[language])) {
+									return null;
+								}
+
+								return (
+									<div key={key} className="flex gap-2">
+										<span className="font-semibold">
+											{label[language]} :
+											<span className="opacity-60 ml-2">
+												{value[language] || value}
+											</span>
+										</span>
+									</div>
+								);
+							}
+						)}
+					</div>
+				</Tile>
+			</div>
 		</Section>
 	);
 }
--- public/data/careerData.tsx
+++ public/data/careerData.tsx
@@ -1,29 +1,137 @@
-export const careerData = [
-	{
-		id: "1",
-		title: {
-			en: "Technical Consultant Dev",
-			fr: "Développeur Consultant Technique"
+export const careerData = {
+	tiles: {
+		timeline: {
+			name: { en: "Timeline", fr: "Timeline" }
 		},
-		company: "LTTD Consulting",
-		description: { en: "", fr: "" },
-		school: "",
-		location: "Saint-Herblain, France",
-		from: "02-05-2025",
-		to: "10-10-2024",
-		techno: "",
-		success: { en: "", fr: "" }
+		details: {
+			name: { en: "Details", fr: "Détails" },
+			contentTitles: {
+				title: { en: "Title", fr: "Titre" },
+				company: { en: "Company", fr: "Entreprise" },
+				school: { en: "School", fr: "École" },
+				location: { en: "Location", fr: "Localisation" },
+				from: { en: "From", fr: "De" },
+				to: { en: "To", fr: "À" },
+				description: { en: "Description", fr: "Description" },
+				techno: { en: "Techno", fr: "Technologies" },
+				success: { en: "Success", fr: "Succès" }
+			}
+		}
 	},
-	{
-		id: "2",
-		title: { en: "Technical Consultant Dev", fr: "" },
-		company: "LTTD Consulting",
-		description: { en: "", fr: "" },
-		school: "",
-		location: "Saint-Herblain, France",
-		from: "02-05-2025",
-		to: "10-10-2024",
-		techno: "",
-		success: { en: "", fr: "" }
-	}
-];
+	entries: [
+		{
+			id: "1",
+			title: {
+				en: "DUT in CS",
+				fr: "DUT Informatique"
+			},
+			company: "",
+			description: { en: [""], fr: [""] },
+			school: "IUT Vannes",
+			location: "Vannes, France",
+			from: "10-09-2019",
+			to: "10-06-2021",
+			techno: ["Java", "SQL", "CS Bases"],
+			success: { en: "", fr: "" }
+		},
+		{
+			id: "2",
+			title: {
+				en: "DevOps intern developer",
+				fr: "Développeur stagiaire DevOps"
+			},
+			company: "Arkea",
+			description: {
+				en: [""],
+				fr: [
+					"Gestion de l'intégration et du déploiement d'outils internes en utilisant Jenkins, Docker, DBMaintain et Liquibase."
+				]
+			},
+			school: "",
+			location: "Nantes, France",
+			from: "10-04-2021",
+			to: "10-06-2021",
+			techno: "",
+			success: { en: "", fr: "" }
+		},
+		{
+			id: "3",
+			title: {
+				en: "Bachelor DevOps",
+				fr: "Bachelor DevOps"
+			},
+			company: "",
+			description: { en: [""], fr: [""] },
+			school: "EPSI Nantes",
+			location: "Saint-Herblain, France",
+			from: "02-10-2021",
+			to: "10-10-2022",
+			techno: "",
+			success: { en: "", fr: "" }
+		},
+		{
+			id: "4",
+			title: { en: "Software Dev Parttime", fr: "Développeur alternant" },
+			company: "SCC",
+			description: {
+				en: [""],
+				fr: [
+					"Conception, développement et mise en œuvre d'un outil d'ingestion des données automatisé en utilisant Azure Data Factory et SQL afin d’optimiser le processus de traitement des données.",
+					"Conception et développement d'applications web, couvrant à la fois le front-end et le back-end, en utilisant HTML/CSS, JavaScript et Node.js.",
+					"Gestion de l'intégration et du déploiement de projets avec des technologies cloud telles que AWS, avec une utilisation de GitHub Actions pour assurer une livraison continue."
+				]
+			},
+			school: "",
+			location: "Saint-Herblain, France",
+			from: "02-10-2021",
+			to: "10-10-2022",
+			techno: "",
+			success: { en: "", fr: "" }
+		},
+		{
+			id: "5",
+			title: { en: "Master in CS : EISI", fr: "Master EISI" },
+			company: "",
+			description: { en: [""], fr: [""] },
+			school: "EPSI Nantes",
+			location: "Nantes, France",
+			from: "02-10-2022",
+			to: "10-10-2024",
+			techno: "",
+			success: { en: "", fr: "" }
+		},
+		{
+			id: "6",
+			title: {
+				en: "Technical Consultant Dev Parttime",
+				fr: "Développeur Consultant Technique alternant"
+			},
+			company: "LTTD Consulting",
+			description: {
+				en: [""],
+				fr: [
+					"Conception et développement d'applications web en Angular, utilisées en tant qu’écrans personnalisés déployés dans l'ERP Infor M3 au sein d’une équipe de développeurs.",
+					"Collaboration étroite avec les clients pour la gestion des projets futurs et les mises à jour des applications déjà déployées."
+				]
+			},
+			school: "",
+			location: "Saint-Herblain, France",
+			from: "02-05-2023",
+			to: "10-10-2024",
+			techno: "",
+			success: { en: "", fr: "" }
+		},
+		{
+			id: "7",
+			title: { en: "Self Learning AI", fr: "IA en autodidacte" },
+			company: "",
+			description: { en: [""], fr: [""] },
+			school: "Calvin's School",
+			location: "Nantes, France",
+			from: "25-01-2025",
+			to: "??-??-????",
+			techno: "",
+			success: { en: "", fr: "" }
+		}
+	]
+};
