229f98fe4000f7d74155362b83dc27b2475ebd83
calvinchiff <calel44jke@gmail.com>
2025-03-31 16:05:13 +0200

    update: content in careerData

--- public/data/careerData.tsx
+++ public/data/careerData.tsx
@@ -13,7 +13,7 @@ export const careerData = {
 				from: { en: "From", fr: "De" },
 				to: { en: "To", fr: "À" },
 				description: { en: "Description", fr: "Description" },
-				techno: { en: "Techno", fr: "Technologies" },
+				techno: { en: "Techno", fr: "Techno" },
 				success: { en: "Success", fr: "Succès" }
 			}
 		}
@@ -26,12 +26,15 @@ export const careerData = {
 				fr: "DUT Informatique"
 			},
 			company: "",
-			description: { en: [""], fr: [""] },
+			description: {
+				en: "That's where I learned the basics in CS and completed my first projects.",
+				fr: "C'est là que j'ai pu apprendre les bases en informatique et réalisé mes premiers projets."
+			},
 			school: "IUT Vannes",
 			location: "Vannes, France",
 			from: "10-09-2019",
 			to: "10-06-2021",
-			techno: ["Java", "SQL", "CS Bases"],
+			techno: "Java, SQL, CS Bases, HTML/CSS",
 			success: { en: "", fr: "" }
 		},
 		{
@@ -42,16 +45,14 @@ export const careerData = {
 			},
 			company: "Arkea",
 			description: {
-				en: [""],
-				fr: [
-					"Gestion de l'intégration et du déploiement d'outils internes en utilisant Jenkins, Docker, DBMaintain et Liquibase."
-				]
+				en: "My first steps into the professional world: I participated in the integration and deployment of internal tools, automating processes with Jenkins, Docker, DBMaintain, and Liquibase.",
+				fr: "Mes premiers pas dans le monde professionnel, j’ai participé à l’intégration et au déploiement d’outils internes en automatisant les process avec Jenkins, Docker, DBMaintain et Liquibase."
 			},
 			school: "",
 			location: "Nantes, France",
 			from: "10-04-2021",
 			to: "10-06-2021",
-			techno: "",
+			techno: "Java, Jenkins, Docker, DBMaintain, Liquibase",
 			success: { en: "", fr: "" }
 		},
 		{
@@ -61,38 +62,44 @@ export const careerData = {
 				fr: "Bachelor DevOps"
 			},
 			company: "",
-			description: { en: [""], fr: [""] },
+			description: {
+				en: "During my DevOps Bachelor’s degree, I deepened my knowledge of software development, microservices, and API design. I also learned to set up CI/CD pipelines to automate deployments. Through various projects, I developed a professional mindset by considering real-world constraints, especially with Agile methodologies.",
+				fr: "Lors de mon Bachelor DevOps, j’ai approfondi le développement logiciel, les microservices et la conception d’API. J’ai également appris à mettre en place des pipelines CI/CD pour automatiser le déploiement. À travers divers projets, j’ai acquis une approche professionnelle en prenant en compte les contraintes réelles du métier, notamment avec les méthodes Agile."
+			},
 			school: "EPSI Nantes",
 			location: "Saint-Herblain, France",
 			from: "02-10-2021",
 			to: "10-10-2022",
-			techno: "",
-			success: { en: "", fr: "" }
+			techno:
+				"React, HTML/CSS, Node.js, SQL, Git, CI/CD, DevOps (Docker, Kubernetes)",
+			success: {
+				en: "",
+				fr: ""
+			}
 		},
 		{
 			id: "4",
 			title: { en: "Software Dev Parttime", fr: "Développeur alternant" },
 			company: "SCC",
 			description: {
-				en: [""],
-				fr: [
-					"Conception, développement et mise en œuvre d'un outil d'ingestion des données automatisé en utilisant Azure Data Factory et SQL afin d’optimiser le processus de traitement des données.",
-					"Conception et développement d'applications web, couvrant à la fois le front-end et le back-end, en utilisant HTML/CSS, JavaScript et Node.js.",
-					"Gestion de l'intégration et du déploiement de projets avec des technologies cloud telles que AWS, avec une utilisation de GitHub Actions pour assurer une livraison continue."
-				]
+				en: "During my apprenticeship, I designed, developed, and deployed an automated data ingestion tool using Azure Data Factory and SQL to optimize data processing. I also worked on full-stack web applications with HTML/CSS, JavaScript, and Node.js. Lastly, I contributed to the integration and deployment of cloud-based projects using AWS, implementing CI/CD pipelines with GitHub Actions to ensure continuous delivery.",
+				fr: "Pendant mon alternance, j’ai conçu, développé et déployé un outil d’ingestion de données automatisé avec Azure Data Factory et SQL pour optimiser le traitement des données. J’ai aussi travaillé sur des applications web full-stack en utilisant HTML/CSS, JavaScript et Node.js. Enfin, j’ai participé à l’intégration et au déploiement de projets cloud avec AWS, en mettant en place des pipelines CI/CD via GitHub Actions pour garantir une livraison continue."
 			},
 			school: "",
 			location: "Saint-Herblain, France",
 			from: "02-10-2021",
 			to: "10-10-2022",
-			techno: "",
+			techno: "HTML/CSS, JavaScript, Node.js, GitHub Actions, Azure, SQL",
 			success: { en: "", fr: "" }
 		},
 		{
 			id: "5",
 			title: { en: "Master in CS : EISI", fr: "Master EISI" },
 			company: "",
-			description: { en: [""], fr: [""] },
+			description: {
+				en: "This program allowed me to deepen my expertise in IT and information systems. I learned to analyze a company’s IT needs, manage projects in an agile environment, and design data strategies aligned with business goals. I also worked on developing applications across different environments (mobile, embedded, ERP) and on integrating, securing, and maintaining cloud and virtualized solutions.",
+				fr: "Ce cursus m’a permis d’approfondir mon expertise en informatique et en systèmes d’information. J’y ai appris à analyser les besoins SI d’une entreprise, piloter des projets en environnement agile et concevoir des stratégies data alignées avec les enjeux business. J’ai également travaillé sur le développement d’applications dans divers environnements (mobiles, embarqués, ERP) et sur l’intégration, la sécurisation et la maintenance de solutions cloud et virtualisées."
+			},
 			school: "EPSI Nantes",
 			location: "Nantes, France",
 			from: "02-10-2022",
@@ -108,29 +115,30 @@ export const careerData = {
 			},
 			company: "LTTD Consulting",
 			description: {
-				en: [""],
-				fr: [
-					"Conception et développement d'applications web en Angular, utilisées en tant qu’écrans personnalisés déployés dans l'ERP Infor M3 au sein d’une équipe de développeurs.",
-					"Collaboration étroite avec les clients pour la gestion des projets futurs et les mises à jour des applications déjà déployées."
-				]
+				en: "Designed and developed web applications in Angular, used as custom screens within the Infor M3 ERP, as part of a development team. Beyond development, I was actively involved in project management, directly handling client needs, tracking requests, and managing updates for deployed applications.",
+				fr: "Conception et développement d’applications web en Angular, utilisées comme écrans personnalisés intégrés à l’ERP Infor M3, au sein d’une équipe de développeurs. En plus du développement, j’ai participé à la gestion des projets en étant en contact direct avec les clients pour recueillir leurs besoins, suivre les demandes et assurer les mises à jour des applications déjà déployées."
 			},
 			school: "",
 			location: "Saint-Herblain, France",
 			from: "02-05-2023",
 			to: "10-10-2024",
-			techno: "",
+			techno: "Angular, SQL, ERP Infor M3, Java",
 			success: { en: "", fr: "" }
 		},
 		{
 			id: "7",
 			title: { en: "Self Learning AI", fr: "IA en autodidacte" },
 			company: "",
-			description: { en: [""], fr: [""] },
+			description: {
+				en: "I’m working on personal projects in Next.js with TailwindCSS and exploring embedded systems with STM32. At the same time, I’m learning AI and MLOPS to apply it to robotics and/or the automotive industry.",
+				fr: "Je travaille sur des projets perso en Next.js avec TailwindCSS et j’explore l’embarqué avec des STM32. En parallèle, je me forme à l’IA et au MLOPS pour l’appliquer à la robotique et/ou l’automobile."
+			},
 			school: "Calvin's School",
 			location: "Nantes, France",
 			from: "25-01-2025",
 			to: "??-??-????",
-			techno: "",
+			techno:
+				"Next.js, React, TailwindCSS, C/C++, STM32, FreeTROS, Python, TensorFlow, PyTorch, scikit-learn, Docker, Kubernetes",
 			success: { en: "", fr: "" }
 		}
 	]
