793148452380b74e7ef8d1210f3ca0cf1aa1e855
calvinchiff <calel44jke@gmail.com>
2025-04-01 15:16:41 +0200

    feat: ProjectsSection project selection via arrows and categories working

--- app/components/sections/ProjectsSection.tsx
+++ app/components/sections/ProjectsSection.tsx
@@ -1,28 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Tile from "@/app/components/ui/Tile";
 import Section from "@/app/components/ui/Section";
 import { projectsData } from "@/public/data/projectsData";
 import { useLanguage } from "@/app/components/lib/LanguageContext";
 
+type Category = "dev" | "craft";
+
 export default function ProjectsSection() {
 	const { language } = useLanguage();
+	const categories: Category[] = ["dev", "craft"];
+
+	const [activeCategory, setActiveCategory] = useState<Category>("dev");
+	const [projectIndex, setProjectIndex] = useState(0);
+
+	const currentProjects = projectsData[activeCategory];
+
+	useEffect(() => {
+		setProjectIndex(0);
+	}, [activeCategory]);
+
+	const handleCategoryChange = (category: Category) => {
+		setActiveCategory(category);
+	};
+
+	const handleNext = () => {
+		setProjectIndex((prevIndex) => (prevIndex + 1) % currentProjects.length);
+	};
+
+	const handlePrevious = () => {
+		setProjectIndex(
+			(prevIndex) =>
+				(prevIndex - 1 + currentProjects.length) % currentProjects.length
+		);
+	};
+
 	return (
-		<Section customClassName="grid-rows-20 grid-cols-4" id="projects">
-			<Tile
-				title={projectsData[0].title[language]}
-				customClassName="col-span-4 row-span-17"
-			>
-				Projet test
-			</Tile>
-			<Tile
-				title="Dev"
-				customClassName="col-span-2 row-span-3 cursor-pointer"
-			></Tile>
-			<Tile
-				title="Craft"
-				customClassName="col-span-2 row-span-3 cursor-pointer"
-			></Tile>
+		<Section customGridClassName="" id="projects">
+			<div className="flex flex-col h-full gap-2">
+				<div className="relative h-full">
+					<button
+						onClick={handlePrevious}
+						className="w-10 h-10 md:w-20 md:h-20 absolute z-50 left-2 md:-left-18 top-1/2 -translate-y-1/2 p-2 cursor-pointer transition-transform duration-200 hover:-translate-x-2 opacity-70 hover:opacity-100"
+					>
+						<Image
+							src="/projects/arrow.png"
+							alt="Previous"
+							objectFit="contain"
+							fill
+						/>
+					</button>
+
+					<button
+						onClick={handleNext}
+						className="w-10 h-10 md:w-20 md:h-20 absolute z-50 right-2 md:-right-18 top-1/2 -translate-y-1/2 p-2 cursor-pointer transition-transform duration-200 hover:translate-x-2 opacity-70 hover:opacity-100"
+					>
+						<Image
+							src="/projects/arrow.png"
+							alt="Next"
+							objectFit="contain"
+							fill
+							className="transform rotate-180"
+						/>
+					</button>
+
+					<Tile
+						title={`${currentProjects[projectIndex].title[language]} #${currentProjects[projectIndex].id}`}
+						customClassName=""
+					/>
+				</div>
+
+				<div className="flex flex-row gap-2 md:h-[70px]">
+					{categories.map((category) => (
+						<Tile
+							key={category}
+							onClick={() => handleCategoryChange(category)}
+							customClassName="cursor-pointer"
+						>
+							<h1
+								className={`transition-all duration-150 ${
+									activeCategory === category
+										? "opacity-90 text-glow font-semibold"
+										: "opacity-40"
+								}`}
+							>
+								{category.charAt(0).toUpperCase() + category.slice(1)}
+							</h1>
+						</Tile>
+					))}
+				</div>
+			</div>
 		</Section>
 	);
 }
--- public/data/projectsData.tsx
+++ public/data/projectsData.tsx
@@ -1,20 +1,45 @@
-export const projectsData = [
-	{
-		id: "1",
-		title: { en: "Portfolio", fr: "Portfolio" },
-		description: { en: "", fr: "" },
-		link: "Saint-Herblain, France",
-		date: "02-05-2025",
-		techno: "",
-		state: { en: "", fr: "" }
-	},
-	{
-		id: "2",
-		title: { en: "Water level sensor", fr: "Détecteur de niveau d'eau" },
-		description: { en: "", fr: "" },
-		link: "Saint-Herblain, France",
-		date: "02-05-2025",
-		techno: "",
-		state: { en: "", fr: "" }
-	}
-];
+export const projectsData = {
+	dev: [
+		{
+			id: "1",
+			title: { en: "Portfolio", fr: "Portfolio" },
+			description: {
+				en: "It's the portfolio you are watching rn !",
+				fr: "C'est le portfolio que vous regardez en ce moment même !"
+			},
+			link: "",
+			date: "03-2025",
+			techno: "Next.js, Tailwind CSS",
+			state: { en: "", fr: "" }
+		},
+		{
+			id: "2",
+			title: { en: "Water level sensor", fr: "Détecteur de niveau d'eau" },
+			description: { en: "", fr: "" },
+			link: "Saint-Herblain, France",
+			date: "02-05-2025",
+			techno: "",
+			state: { en: "", fr: "" }
+		}
+	],
+	craft: [
+		{
+			id: "1",
+			title: { en: "Wooden table", fr: "Table en bois" },
+			description: { en: "", fr: "" },
+			link: "Saint-Herblain, France",
+			date: "02-05-2025",
+			techno: "",
+			state: { en: "", fr: "" }
+		},
+		{
+			id: "2",
+			title: { en: "Robot volant", fr: "Robot volant" },
+			description: { en: "", fr: "" },
+			link: "Saint-Herblain, France",
+			date: "02-05-2025",
+			techno: "",
+			state: { en: "", fr: "" }
+		}
+	]
+};
