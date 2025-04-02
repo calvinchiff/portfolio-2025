082dd4da6a66913e42fd8e815eab7c981950e533
calvinchiff <calel44jke@gmail.com>
2025-04-02 17:46:29 +0200

    feat: Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Exo } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const exo = Exo({
 	variable: "--font-exo",
@@ -48,7 +49,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className="scroll-smooth">
-			<body className={`${exo.variable} antialiased`}>{children}</body>
+			<body className={`${exo.variable} antialiased`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
