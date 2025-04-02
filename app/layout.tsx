e68b87527afbf7c63a54b5ed3f646c88ccaefb47
calvinchiff <calel44jke@gmail.com>
2025-04-02 15:26:38 +0200

    update: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,35 @@ const exo = Exo({
 export const metadata: Metadata = {
 	title: "Calvin Chiffot - Portfolio",
 	description:
-		"Portfolio Calvin Chiffot Full-Stack NodeJS JavaScript TypeScript Dev"
+		"Portfolio Calvin Chiffot Full-Stack NodeJS JavaScript TypeScript Dev",
+	icons: {
+		icon: [
+			{ url: "/favicon_io/favicon.ico" },
+			{
+				url: "/favicon_io/favicon-16x16.png",
+				sizes: "16x16",
+				type: "image/png"
+			},
+			{
+				url: "/favicon_io/favicon-32x32.png",
+				sizes: "32x32",
+				type: "image/png"
+			}
+		],
+		apple: [{ url: "/favicon_io/apple-touch-icon.png" }],
+		other: [
+			{
+				url: "/favicon_io/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png"
+			},
+			{
+				url: "/favicon_io/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png"
+			}
+		]
+	}
 };
 
 export default function RootLayout({
