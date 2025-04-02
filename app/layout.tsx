893e5b697cfcc92513410b3babd4f0a1d3381860
calvinchiff <calel44jke@gmail.com>
2025-04-02 17:52:23 +0200

    feat: Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Exo } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const exo = Exo({
 	variable: "--font-exo",
@@ -52,6 +53,7 @@ export default function RootLayout({
 			<body className={`${exo.variable} antialiased`}>
 				{children}
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
